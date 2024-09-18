56e002b42ea0a9135db6a264a0b38537a70d8920
Hatice Angılcı <angilcihatice@gmail.com>
2024-09-18 12:54:52 +0300

    V3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Versiyon 1");
         System.out.println("Versiyon 2");
+        System.out.println("Versiyon 3");
     }
 }
